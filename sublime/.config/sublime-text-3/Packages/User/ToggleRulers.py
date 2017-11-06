4a8c3d5e82b63b6d46d9cbe4c3f141a4d0d7c0d3
Tomaszal <mrtomaszal@gmail.com>
2017-11-06 23:05:09 +0200

    Added Sublime configs.
    Signed-off-by: Tomaszal <mrtomaszal@gmail.com>

---
+++ sublime/.config/sublime-text-3/Packages/User/ToggleRulers.py
@@ -0,0 +1,8 @@
+import sublime
+import sublime_plugin
+
+
+class ToggleRulersCommand(sublime_plugin.TextCommand):
+	def run(self, edit, **kwargs):
+		rulers = kwargs["values"] if self.view.settings().get("rulers") == [] else []
+		self.view.settings().set("rulers", rulers)
