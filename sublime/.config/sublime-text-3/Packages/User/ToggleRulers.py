c25d65399cc48b9fb30c935d0a635a5d87d5db7d
Tomaszal <mrtomaszal@gmail.com>
2018-07-09 19:14:03 +0300

    Removed Sublime configs and added VSCode configs

--- sublime/.config/sublime-text-3/Packages/User/ToggleRulers.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class ToggleRulersCommand(sublime_plugin.TextCommand):
-	def run(self, edit, **kwargs):
-		rulers = kwargs["values"] if self.view.settings().get("rulers") == [] else []
-		self.view.settings().set("rulers", rulers)
