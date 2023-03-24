02dcac2cd9bec551fef314c8037680ce53ed5adf
Tomas Zaluckij <tz@tomaszal.com>
2023-03-24 05:20:28 +0000

    feat: add gpg integration to piv agent

--- configuration/programs/piv-agent.nix
+++ configuration/programs/piv-agent.nix
@@ -1,6 +1,8 @@
 {
   self,
+  config,
   pkgs,
+  lib,
   ...
 }: let
   piv-agent = pkgs.buildGoModule {
@@ -15,13 +17,13 @@ in {
 
   hm.home.packages = [piv-agent pkgs.pinentry-qt];
 
-  environment.variables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/piv-agent/ssh.socket";
-
+  # PIV agent service
   hm.systemd.user.services.piv-agent = {
     Unit.Description = "piv-agent service";
     Service.ExecStart = "${piv-agent}/bin/piv-agent serve --agent-types=ssh=0;gpg=1 --touch-notify-delay=1s";
   };
 
+  # PIV agent socket activation for SSH and GPG
   hm.systemd.user.sockets.piv-agent = {
     Unit = {
       Description = "piv-agent socket activation";
@@ -31,9 +33,21 @@ in {
 
     Install.WantedBy = ["sockets.target"];
 
-    Socket.ListenStream = [
-      "%t/piv-agent/ssh.socket"
-      "%t/gnupg/S.gpg-agent"
-    ];
+    Socket = {
+      ListenStream = [
+        "%t/piv-agent/ssh.socket"
+        "%t/gnupg/S.gpg-agent"
+      ];
+
+      # GPG is picky about permissions
+      DirectoryMode = "0700";
+    };
   };
+
+  # SSH integration
+  hm.home.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/piv-agent/ssh.socket";
+
+  # GPG integration
+  hm.home.file."${config.hm.programs.gpg.homedir}/gpg.conf".text = lib.mkForce "no-autostart";
+  hm.services.gpg-agent.enable = lib.mkForce false;
 }
