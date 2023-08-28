f144d85968ec2fb2380505c04af37779cda4320b
Tomas Zaluckij <tz@tomaszal.com>
2023-08-28 16:30:27 +0100

    fix: move Hyprland Cachix config to be available in the installer

--- configuration/nix.nix
+++ configuration/nix.nix
@@ -3,8 +3,14 @@
   config,
   ...
 }: {
-  # Enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    # Enable flakes
+    experimental-features = ["nix-command" "flakes"];
+
+    # Enable Hyprland Cachix
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   # Allow configured unfree packages
   nixpkgs.config.allowUnfreePredicate = pkg:
--- configuration/programs/hyprland.nix
+++ configuration/programs/hyprland.nix
@@ -19,12 +19,6 @@
     playerctl
   ];
 
-  # Enable Hyprland Cachix
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
