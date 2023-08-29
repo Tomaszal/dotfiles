62267d2686b4961bb2f82ecc5ae5c002305c4721
Tomas Zaluckij <tz@tomaszal.com>
2023-08-29 23:07:17 +0100

    feat: setup main pc hardware

---
+++ hardware-configuration/pc-main-amd64.nix
@@ -0,0 +1,25 @@
+{
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+
+  # AMD CPU fixes
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # Nvidia GPU fixes
+  services.xserver.videosDrivers = ["nvidia"];
+  hardware.nvidia.modesetting.enable = true;
+  programs.hyprland.enableNvidiaPatches = true;
+  hm.wayland.windowManager.hyprland.enableNvidiaPatches = true;
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+}
