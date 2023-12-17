c2bc59b0fcee13f1aac813361c2b724f4325e8ee
Tomas Zaluckij <tz@tomaszal.com>
2023-12-17 13:43:29 +0000

    feat: disable natural sort order

--- src/locale.nix
+++ src/locale.nix
@@ -1,4 +1,9 @@
 {
   time.timeZone = "Europe/London";
+
+  # Enable all locales
   i18n.supportedLocales = ["all"];
+
+  # Disable natural sort order
+  i18n.extraLocaleSettings.LC_COLLATE = "C";
 }
