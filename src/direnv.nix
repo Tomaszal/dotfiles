0cfb203b55a9a52581fd0a151fd76e2353eed5d9
Tomas Zaluckij <tz@tomaszal.com>
2024-03-13 23:58:58 +0000

    feat: improve direnv config

--- src/direnv.nix
+++ src/direnv.nix
@@ -2,5 +2,19 @@
   hm.programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+
+    # https://direnv.net/man/direnv.toml.1.html#global
+    config.global = {
+      # Load `.envrc` with `set -euo pipefail`
+      # TODO: remove when this option becomes default upstream
+      strict_env = true;
+
+      # Specify how long to wait before warning the user that the command is
+      # taking too long to execute
+      warn_timeout = "1m";
+
+      # Hide the diff of the environment variables when loading the `.envrc`
+      hide_env_diff = true;
+    };
   };
 }
