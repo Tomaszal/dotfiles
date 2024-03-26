431051febc402b01b065375aa828da7329d59342
Tomas Zaluckij <me@tomaszal.com>
2024-03-26 23:27:21 +0000

    chore: update

--- src/zsh.nix
+++ src/zsh.nix
@@ -4,7 +4,7 @@
 
   hm.programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
