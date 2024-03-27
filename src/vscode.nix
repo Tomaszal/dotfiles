6a4da729068851f50577e38647530335e6c2d983
Tomas Zaluckij <me@tomaszal.com>
2024-03-27 00:36:47 +0000

    feat: set vscode as default editor for cli operations

--- src/vscode.nix
+++ src/vscode.nix
@@ -1,4 +1,13 @@
 {
   allowUnfreePackages = ["vscode"];
   hm.programs.vscode.enable = true;
+
+  # Might want to revert to `nano` for ssh connections
+  # https://stackoverflow.com/a/57144660
+  hm.home.sessionVariables = let
+    cmd = "code --wait";
+  in {
+    EDITOR = cmd;
+    VISUAL = cmd;
+  };
 }
