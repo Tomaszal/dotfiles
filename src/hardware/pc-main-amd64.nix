deff28cee756f02e51ab0cd3931c1d698bd20fc0
Tomas Zaluckij <me@tomaszal.com>
2025-08-04 17:03:30 +0100

    Remove unused VM hardware, simplify PC hardware using NixOS Hardware flake

--- src/hardware/pc-main-amd64.nix
+++ src/hardware/pc-main-amd64.nix
@@ -1,35 +1,41 @@
-{
+{inputs, ...}: {
   nixpkgs.hostPlatform = "x86_64-linux";
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
+  boot.supportedFilesystems = ["ntfs"];
+
+  security.polkit.enable = true;
+
+  imports = with inputs.nixos-hardware.nixosModules; [
+    common-pc
+    common-pc-ssd
+
+    common-cpu-amd
+    common-cpu-amd-pstate
+    common-cpu-amd-zenpower
+
+    "${inputs.nixos-hardware}/common/gpu/nvidia/ampere"
+  ];
+
+  nixpkgs.config.cudaSupport = true;
+
+  allowUnfreePackages = ["nvidia-x11" "nvidia-settings"];
 
   # Generate boot entries for other OS on the system, enabling dual boot
   boot.loader.grub.useOSProber = true;
 
-  # AMD CPU fixes
-  hardware.enableRedistributableFirmware = true;
-  hardware.cpu.amd.updateMicrocode = true;
-
-  # Nvidia GPU fixes
-  allowUnfreePackages = ["nvidia-x11" "nvidia-settings"];
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    nvidiaSettings = true;
-    open = false;
-    powerManagement.enable = true;
-  };
-
-  # Enable OpenGL
-  hardware.opengl = {
+  # Enable accelerated graphics
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   # Mount data disk on login
   udisks.mounts = [{block-device = "/dev/disk/by-label/data";}];
+
+  # Enable system emulation for cross-compilation
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 }
