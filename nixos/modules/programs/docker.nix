70d09cf18aacf6e80ce04c706644729beea09916
Tomas Zaluckij <me@tomaszal.com>
2026-04-10 10:22:17 +0100

    Switch from Podman to Docker for better compatibility

--- nixos/modules/programs/docker.nix
+++ nixos/modules/programs/docker.nix
@@ -1,18 +1,9 @@
 {config, ...}: {
-  virtualisation.podman = {
-    enable = true;
+  virtualisation.docker.enable = true;
 
-    # Create an alias mapping `docker` to `podman`
-    dockerCompat = true;
+  # Allow main user to run docker without sudo
+  users.groups.docker.members = [config.user.username];
 
-    # Make the Podman socket available in place of the Docker socket, so Docker
-    # tools can find the Podman socket
-    dockerSocket.enable = true;
-
-    # Required for containers under podman-compose to be able to talk to each other
-    defaultNetwork.settings.dns_enabled = true;
-  };
-
-  # Allow main user to run podman without sudo
-  users.groups.podman.members = [config.user.username];
+  # https://github.com/NixOS/nixpkgs/issues/417641
+  networking.firewall.trustedInterfaces = ["br+"];
 }
